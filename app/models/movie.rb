37a29d9d950b26f3ff310bc0104ec24f21341568
EC2 Default User <ec2-user@ip-172-31-94-48.ec2.internal>
2021-02-25 22:51:22 +0000

    part 2 filtering complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,14 @@
 class Movie < ActiveRecord::Base
-  def self.all_ratings
-    return [G, PG, PG-13 ,R]
-  end
+
+    def self.all_ratings
+      return ['G', 'PG', 'PG-13' ,'R'] #collection of movie ratings
+    end
+    
+    def self.with_ratings(ratings)
+        if ratings.nil? #no rating is selected
+            Movie.all
+        else #filetering by a particular rating
+            Movie.where(rating:ratings)
+        end
+    end
 end
